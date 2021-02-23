cef726a7f525cd269f89851cb526757c11e08264
Paris DOUADY <paris.douady@hotmail.fr>
2021-02-23 12:40:52 +0100

    add legion bug

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use rodio::Source;
+use legion::WorldOptions;
 
 fn main() {
-    let (a, b) = rodio::OutputStream::try_default().unwrap();
-    let buf = std::fs::read("music1.ogg").ok().unwrap();
-
-    let s = rodio::Decoder::new(std::io::Cursor::new(buf)).unwrap();
-    b.play_raw(s.convert_samples()).unwrap();
-    loop {}
+    let mut w = legion::world::World::new(WorldOptions::default());
+    w.push((1,));
 }
